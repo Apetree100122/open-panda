80c4e827eed83e6acd7817617d902cda1a843e28
Alexander Petree <apetree1001@email.phoenix.edu>
2025-02-01 07:09:46 -0800

    Update and rename package.json to package.js

---
+++ package.js
@@ -0,0 +1,65 @@
+{
+  "name": "open-panda",
+  "version": "0.1.0","description": "Open Panda, a platform for the worlds largest open datasets, stored on a decentralized network","author": "apetree100122<apetree1001@email.phoenix.edu>","license": "Apache-2.0", "scripts": {"dev-fe": "npm run dev -w fe", "dev-be": "npm run dev -w be"}, "repository": {"type": "git","url":
+	"git@github.com:data-preservation-programs/openpanda.git" }, "engines":
+	{"node": ">=16.18.0 },
+  "bugs": { "url": "https://github.com/data-preservation-programs/openpanda/issues"},
+	"homepage": "https://github.com/data-preservation-programs/openpanda
+	#readme",
+"workspaces,
+	["packages/fe",
+	"packages/be"
+	]
+}
+
+
+	
+Copyright 2025 "alexander petree<apetree1001@email.phoenix.edu>"Agency Undone<hello@agencyundone.com>,Licensed under the Apache License, Version 2.0; you may not use this
+file except in compliance with the License.
+You may obtain a copy
+of the License at    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by
+applicable law or agreed 
+to in writing, software
+distributed
+under the License 
+is distributed on an "AS IS"
+BASIS, WITHOUT WARRANTIES 
+OR CONDITIONS OF ANY KIND, 
+either express or implied.
+See the License
+for the specific language 
+governing permissions and
+limitations 
+under the License
+
+
+
+
+	
+
+	changed from:
+	// "name": "open-panda",
+//  "version": "0.1.0",
+//  "description": "Open Panda, a platform for the world's largest open datasets, stored on a decentralized network",
+//  "author": {
+//  "name": "Agency Undone",
+ //   "email": "hello@agencyundone.com"},
+ // "license": "Apache-2.0",
+ // "scripts": {
+ //   "dev-fe": "npm run dev -w fe",
+ //   "dev-be": "npm run dev -w be"  },
+//  "repository": {
+  //  "type": "git",
+  //  "url": "git@github.com:data-preservation-programs/openpanda.git"
+//  },
+//  "engines": {
+//    "node": ">=16.18.0"
+//  },
+//  "keywords": [],
+//  "bugs": {
+ //   "url": "https://github.com/data-preservation programs/openpanda/issues" },
+//  "homepage": "https://github.com/data-preservation-programs/openpanda#readme",
+//  "workspaces": [ "packages/fe",  "packages/be"]
+//
+to the top format 2/1/2025 @ 703am
